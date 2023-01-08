ea91154d20dac97a3e5fd9ff64d86d41137ec079
hamid pahlevani <hamidpahlevani1994@gmail.com>
2023-01-08 17:12:05 +0330

    add pages and routing

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>about</h1>
+        <p>This is an app to find latest dj and musical events</p>
+    </div>
+  )
+}
